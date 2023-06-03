c96ad228110bb259eae704e690974bdce5a7bac1
Victoria Benoit <vrose834@gmail.com>
2023-06-03 14:43:08 -0300

    Added the pictures and started to map the projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,18 @@
+import projectList from "../projectList";
+
 const Projects = () => {
-    return ( <div className="projects">Projects</div> );
+    return ( 
+    <div className="projects">
+        {projectList.map(project => (
+            <div className="project" key={project.id}>
+                <h3>{project.title}</h3>
+            </div>
+        ))}
+
+
+
+
+    </div> );
 }
  
 export default Projects;
---
+++ src/projectList.js
@@ -0,0 +1,49 @@
+
+const projectList = [  
+    { id: 1,
+     title: 'Card Wise',
+     github: 'https://github.com/Pilotguide9897/CardWise',
+     deployed: 'https://dry-badlands-78694.herokuapp.com/',
+     description: 'A Full Stack Group Project, test your knowledge with various flash cards',
+     img: ''
+    },
+     {
+        id: 2,
+        title: 'Javascript Quiz',
+        github: 'https://github.com/roseandlily33/Violet-Quizzlet',
+        deployed: 'https://roseandlily33.github.io/Violet-Quizzlet/',
+        description: 'A front end quiz to test your knowledge',
+        img: ''
+     },
+     {
+        id: 3,
+        title: 'Just a text editor',
+        github: 'https://github.com/roseandlily33/texedDB',
+        deployed: 'https://shrouded-plateau-49137.herokuapp.com/',
+        description: '',
+        img: ''
+     }, {
+        id: 4,
+        title: 'English Football Tracker',
+        github: '',
+        deployed: 'https://roseandlily33.github.io/englishFootballTracker32/',
+        description: 'A Front End Group Project,',
+        img: ''
+     }, {
+        id: 5,
+        title: 'Cloudy with a chance of coding',
+        github: 'https://github.com/roseandlily33/cloudyWithAChanceOfCoding',
+        deployed: 'https://roseandlily33.github.io/cloudyWithAChanceOfCoding/',
+        description: '',
+        img: ''
+     }, {
+        id: 6, 
+        title: 'Shortcake Blog',
+        github: 'https://github.com/roseandlily33/cuddly-happiness',
+        deployed: 'https://whispering-bastion-54456.herokuapp.com/',
+        description: '',
+        img: ''
+     }
+    
+ ];
+export default projectList;
