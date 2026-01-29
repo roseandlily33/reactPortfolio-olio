bb930e4decdc9412f98b7891317a5ccd43aea883
Victoria Benoit <vrose834@gmail.com>
2026-01-29 19:06:12 -0400

    oops - forgot to leave that part in

--- src/routes/AboutMe/ContactForm/Contact.jsx
+++ src/routes/AboutMe/ContactForm/Contact.jsx
@@ -51,7 +51,8 @@ const ContactMe = () => {
     <ContactMeSection>
       <h3>Let’s Build Something Amazing Together</h3>
       <div className="contact-intro">
-        Ready to start your project or have questions? Select a service or just say hello—let’s connect and make something great!
+        Ready to start your project or have questions? Select a service or just
+        say hello—let’s connect and make something great!
       </div>
       <p>Form is currently unavailable</p>
       {/* <ContactRow>
@@ -104,57 +105,58 @@ const ContactMe = () => {
             />
           </form>
         </ContactFormCol>
-        <ContactInfoCol>
-          <ContactInfo>
-            <div className="contact-row-item">
-              <a
-                href="mailto:vrose834@email.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaEnvelope /> <span className="contact-label">Email</span>{" "}
-                vrose834@email.com
-              </a>
-            </div>
-            <div className="contact-row-item">
-              <a
-                href="tel:+19028171001"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaPhoneAlt /> <span className="contact-label">Phone</span> +1
-                (902) 817-1001
-              </a>
-            </div>
-            <div className="icon-link-row">
-              <div className="contact-row-item">
-                <span>
-                  <FaMapMarkerAlt />{" "}
-                  <span className="contact-label">Location</span> Halifax NS, CA
-                </span>
-              </div>
-              <a
-                href="https://github.com/roseandlily33"
-                className="icon-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/victoria-benoit-3rose3/"
-                className="icon-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn"
-              >
-                <FaLinkedin />
-              </a>
-            </div>
-          </ContactInfo>
-        </ContactInfoCol>
+
       </ContactRow> */}
+      <ContactInfoCol>
+        <ContactInfo>
+          <div className="contact-row-item">
+            <a
+              href="mailto:vrose834@email.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaEnvelope /> <span className="contact-label">Email</span>{" "}
+              vrose834@gmail.com
+            </a>
+          </div>
+          <div className="contact-row-item">
+            <a
+              href="tel:+19028171001"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaPhoneAlt /> <span className="contact-label">Phone</span> +1
+              (902) 817-1001
+            </a>
+          </div>
+          <div className="icon-link-row">
+            <div className="contact-row-item">
+              <span>
+                <FaMapMarkerAlt />{" "}
+                <span className="contact-label">Location</span> Halifax NS, CA
+              </span>
+            </div>
+            <a
+              href="https://github.com/roseandlily33"
+              className="icon-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/victoria-benoit-3rose3/"
+              className="icon-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </ContactInfo>
+      </ContactInfoCol>
     </ContactMeSection>
   );
 };
